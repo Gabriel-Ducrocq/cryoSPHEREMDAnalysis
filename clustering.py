97d5a5522702fd2cd95892b618a08a742060e7b5
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-10-28 22:09:47 +0100

    sending to remote

---
+++ clustering.py
@@ -0,0 +1,19 @@
+import glob
+import argparse
+import MDAnalysis as mda
+from MDAnalysis.analysis.align import AlignTraj, AverageStructure
+
+
+
+def clustering(structures_file):
+    """
+    Performs clustering of the structures in the input file.
+    Parameters:
+        - structures_file: str, file containing all the structures to cluster
+    :return: None
+    """
+
+
+
+assert clustering.__doc__
+assert clustering("file.pdb") is None
