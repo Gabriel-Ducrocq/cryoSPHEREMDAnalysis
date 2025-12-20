c38aac82ba6d50ae307a044c25660bdccbb49af9
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-12-20 15:11:24 +0100

    computing the pairwise distances

---
+++ src/pairwise_distances.py
@@ -0,0 +1,40 @@
+import utils
+import sklearn
+import argparse
+
+
+parser_arg = argparse.ArgumentParser()
+parser_arg.add_argument(
+    "--structures_path",
+    type=str,
+    required=True,
+    help="path to the pdb file containing the structure we want to align to",
+)
+parser_arg.add_argument(
+    "--distances_mat",
+    type=str,
+    required=True,
+    help="Path the output npy file containing the linkage matrix",
+)
+
+
+
+def compute_pairwise_distances(structures_path, output_path):
+    """
+    Function computing the pairwise distances between structures
+    :param structures_path: str, path to the pdb file containing all the structures
+    :param output_path: str, path where we want to save the pairwise distances matrix
+    :return: None
+    """
+    all_coords = utils.get_calpha(structures_path)
+    pairwise_dist = sklearn.metrics.pairwise_distances(all_coords)
+    utils.write_file(output_path, pairwise_dist)
+
+
+
+
+if __name__ == "__main__":
+    args = parser_arg.parse_args()
+    structures_path = args.structures_path
+    distances_mat_path = args.distances_mat
+    compute_pairwise_distances(structures_path, distances_mat_path)
